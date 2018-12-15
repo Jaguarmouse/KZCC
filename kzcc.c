37d1149d42799952862ea8fe02abb22b2dae5226
jaguarmouse <kazune.haraguchi.aboon@gmail.com>
2018-12-15 23:47:49 +0900

    tokenized input

--- kzcc.c
+++ kzcc.c
@@ -1,5 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+
+enum {
+  TK_NUM = 256,
+  TK_EOF,
+};
+
+typedef struct {
+  int ty; //type
+  int val; //数値
+  char* input; //エラー用文字列
+} Token;
+
+Token tokens[100];
+
+void tokenize(char* p) {
+  int i;
+  while (*p) {
+    //空白
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+
+    //演算子(+ or -)
+    if (*p == '+' || *p == '-') {
+      tokens[i].ty = *p;
+      tokens[i].input = p;
+      i++;
+      p++;
+      continue;
+    }
+
+    //数値(int)
+    if (isdigit(*p)) {
+      tokens[i].ty = TK_NUM;
+      tokens[i].input = p;
+      tokens[i].val = strtol(p, &p, 10);
+      i++;
+      continue;
+    }
+
+    fprintf(stderr, "トークナイズできません: %s\n", p);
+    exit(1);
+  }
+
+  tokens[i].ty = TK_EOF;
+  tokens[i].input = p;
+}
+
+void error(int i) {
+  fprintf(stderr, "予期せぬトークンです: %s\n", tokens[i].input);
+  exit(1);
+}
 
 int main(int argc, char **argv) {
   if (argc != 2) {
@@ -7,31 +61,40 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  char* p = argv[1];
+  tokenize(argv[1]);
 
   printf(".intel_syntax noprefix\n");
   printf(".global _main\n");
   printf("_main:\n");
-  printf("  mov rax, %ld\n", strtol(p, &p, 10));
 
-  while(*p) {
-    if(*p == '+') {
-      p++;
-      printf("  add rax, %ld\n", strtol(p, &p, 10));
+  if (tokens[0].ty != TK_NUM)
+    error(0);
+
+  printf("  mov rax, %d\n", tokens[0].val);
+
+  int i = 1;
+  while (tokens[i].ty != TK_EOF) {
+    if(tokens[i].ty == '+') {
+      i++;
+      if (tokens[i].ty != TK_NUM)
+        error(i);
+      printf("  add rax, %d\n", tokens[i].val);
+      i++;
       continue;
     }
 
-    if(*p == '-') {
-      p++;
-      printf("  sub rax, %ld\n", strtol(p, &p, 10));
+    if(tokens[i].ty == '-') {
+      i++;
+      if (tokens[i].ty != TK_NUM)
+        error(i);
+      printf("  sub rax, %d\n", tokens[i].val);
+      i++;
       continue;
     }
 
-    fprintf(stderr, "予期せぬ文字です: '%c'\n", *p);
-    return 1;
+    error(i);
   }
 
   printf("  ret\n");
-
   return 0;
 }
