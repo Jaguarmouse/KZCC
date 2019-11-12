9e330d695c640b72c51c0e5dff2257e2cf4fb1d3
jaguarmouse <kazune.haraguchi.aboon@gmail.com>
2019-11-12 22:55:04 +0900

    added syntax tree and code generator for addition and subtraction

--- kzcc.c
+++ kzcc.c
@@ -20,6 +20,21 @@ struct Token {
   char *str;
 };
 
+typedef enum {
+  ND_ADD,
+  ND_SUB,
+  ND_NUM
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node {
+  NodeKind kind;
+  Node *lhs;
+  Node *rhs;
+  int val;
+};
+
 Token *token;
 
 char *user_input;
@@ -36,6 +51,7 @@ void error_at(char *loc, char *fmt, ...) {
   fprintf(stderr, "\n");
   exit(1);
 }
+
 void error(char *fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
@@ -106,6 +122,62 @@ Token *tokenize(char *p) {
   return head.next;
 }
 
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_node_num(int val) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+Node *primary() {
+  return new_node_num(expect_number());
+}
+
+Node *expr() {
+  Node *node = primary();
+
+  for(;;) {
+    if (consume('+'))
+      node = new_node(ND_ADD, node, primary());
+    else if (consume('-'))
+      node = new_node(ND_SUB, node, primary());
+    else
+      return node;
+  }
+}
+
+void gen(Node *node) {
+  if (node->kind == ND_NUM) {
+    printf("  push %d\n", node->val);
+    return;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch (node->kind) {
+  case ND_ADD:
+    printf("  add rax, rdi\n");
+    break;
+  case ND_SUB:
+    printf("  sub rax, rdi\n");
+    break;
+  }
+
+  printf("  push rax\n");
+}
+
 int main(int argc, char **argv) {
   if (argc != 2) {
     error("引数の個数が正しくありません。\n");
@@ -114,23 +186,15 @@ int main(int argc, char **argv) {
 
   user_input = argv[1];
   token = tokenize(user_input);
+  Node *node = expr();
 
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("  main:\n");
 
-  printf("  mov rax, %d\n", expect_number());
+  gen(node);
 
-  while (!at_eof()) {
-    if (consume('+')) {
-      printf("  add rax, %d\n", expect_number());
-      continue;
-    }
-
-    expect('-');
-    printf("  sub rax, %d\n", expect_number());
-  }
-
+  printf("  pop rax\n");
   printf("  ret\n");
   return 0;
 }
